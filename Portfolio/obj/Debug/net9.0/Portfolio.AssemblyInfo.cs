48e97ead01ef458e21135e55b8b100f7a2ffd0e3
DerrekNguyen <mnguye21@kent.edu>
2025-09-06 19:38:48 -0400

    homepage progress

--- Portfolio/obj/Debug/net9.0/Portfolio.AssemblyInfo.cs
+++ Portfolio/obj/Debug/net9.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7934f41148f616fbf8623eb854f5194d40f4f58c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf92b40f364f5ebae844746124145965919fda53")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
