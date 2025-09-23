eeda368fc8e1f59430c03bdd2d0d0abf67ab0967
DerrekNguyen <mnguye21@kent.edu>
2025-09-23 17:49:29 -0400

    initial commit

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import face from './assets/face.jpg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <img src={face}></img>
+      </div>
+    </>
+  )
+}
+
+export default App
