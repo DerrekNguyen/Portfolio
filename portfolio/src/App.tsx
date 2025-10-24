7233d22254f5920f91784b4989efe3c6a99e1d6b
DerrekNguyen <mnguye21@kent.edu>
2025-10-23 20:58:04 -0400

    Please work

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -2,12 +2,12 @@ import Home from './pages/Home'
 import NavBar from './components/NavBar/NavBar';
 import ParticleComponent from './components/Particles';
 import Projects from './pages/Projects';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import './App.css'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/Portfolio">
       <NavBar/>
       <ParticleComponent />
       <Routes>
